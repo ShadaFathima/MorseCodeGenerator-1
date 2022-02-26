85583b9ce03596bcb86b9e864ba1b4d93b457d4a
Nived Narayanan <81697680+nived1967@users.noreply.github.com>
2022-02-27 01:48:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,87 +1,98 @@
 const mapping = {
-  "A": ".-",
-  "B": "-...",
-  "C": "-.-.",
-  "D": "-..",
-  "E": ".",
-  "F": "..-.",
-  "G": "--.",
-  "H": "....",
-  "I": "..",
-  "J":".--",
-  "K":"-.-",
-  "L":".-..",
-  "M":"--",
-  "N":"-.",
-  "O":"---",
-  "P":".--.",
-  "Q":"--.-",
-  "R":"._.",
-  "S":"...",
-  "T":"-",
-  "U":"..-",
-  "V":"...-",
-  "W":".--",
-  "X":"-..-",
-  "Y":"-.--",
-  "Z":"--..",
+  A: ".-",
+  B: "-...",
+  C: "-.-.",
+  D: "-..",
+  E: ".",
+  F: "..-.",
+  G: "--.",
+  H: "....",
+  I: "..",
+  J: ".--",
+  K: "-.-",
+  L: ".-..",
+  M: "--",
+  N: "-.",
+  O: "---",
+  P: ".--.",
+  Q: "--.-",
+  R: "._.",
+  S: "...",
+  T: "-",
+  U: "..-",
+  V: "...-",
+  W: ".--",
+  X: "-..-",
+  Y: "-.--",
+  Z: "--..",
 
-  "0":"-----",
-  "1":".----",
-  "2":"..--",
-  "3":"...--",
-  "4":"....-",
-  "5":".....",
-  "6":"-....",
-  "7":"--...",
-  "8":"---..",
-  "9":"----.",
+  0: "-----",
+  1: ".----",
+  2: "..--",
+  3: "...--",
+  4: "....-",
+  5: ".....",
+  6: "-....",
+  7: "--...",
+  8: "---..",
+  9: "----.",
 
-  "?":"..--.."
+  "?": "..--..",
 };
 var code;
-function texttomorse()
-{
-    let input=document.getElementById("input").value;
+function texttomorse() {
+  let input = document.getElementById("input").value;
 
-    input=input.toUpperCase();
+  input = input.toUpperCase();
 
-    let arr1=input.split("");
+  let arr1 = input.split("");
 
-    let arr2=arr1.map(x => {
-        if(mapping[x])
-        {
-            return mapping[x];
-        }
+  let arr2 = arr1.map((x) => {
+    if (mapping[x]) {
+      return mapping[x];
+    } else {
+      return "/";
+    }
+  });
 
-        else
-        {
-            return '/';
-        }
-    });
-
-    code=arr2.join(" ");
-    document.getElementById("output").value=code;
+  code = arr2.join(" ");
+  document.getElementById("output").value = code;
 }
 
-function playmorse(e)
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+var audio1=document.getElementById('a1');
+var audio2=document.getElementById('a2');
+var audio3=document.getElementById('a3');
+var p=0;
+var c=0;
+
+function pause(e)
 {
-    var audio1=document.getElementById('a1');
-    var audio2=document.getElementById('a2');
-    var audio3=document.getElementById('a3');
+    p=1;
+    c++;
+}
+
+async function playmorse(e)
+{
     if(!code)
     return;
     console.log(code.length);
-    for(var i=0;i<code.length;i++)
+    for(let i=0;i<code.length;i++)
     {
-        // console.log(code[i])
+        if(p==1 && c==1)
+        {
+            c=0;
+            break;
+        }
+
         if(code[i]==='.')
         {
             console.log('block1:'+i)
             audio1.currentTime=0;
             audio1.play();
-            audio3.currentTime=0;
-            audio3.play();
         }
 
         else if(code[i]==='-')
@@ -89,8 +100,6 @@ function playmorse(e)
             console.log('block2:'+i)
             audio2.currentTime=0;
             audio2.play();
-            audio3.currentTime=0;
-            audio3.play();
         }
 
         else if(code[i]=='/')
@@ -99,23 +108,9 @@ function playmorse(e)
             audio3.currentTime=0;
             audio3.play();
         }
+
+        await sleep(420)
+
     }
-
-    // console.log(audio1);
-    // console.log(audio2);
-    // audio1.play();
-    // audio1.play();
-    // audio3.play();
-    // audio2.play();
-    // if(!code)
-    // return;
-
-    // code.forEach(p => {
-        
-    // });
-    // if(!audio)
-    //     return;
-    // audio.currentTime=0;
-    // audio.play();
 
 }
